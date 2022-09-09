7c4bf5c2743762976e45b7c94eee289ea84a5cf5
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-09 15:00:58 +0300

    add more css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-(function(){
-    
+(function () {
+  window.onscroll = function () {
+    myFunction();
+  };
 
-}());
+  let navbar = document.querySelector("nav");
+  let sectionIntro = document.getElementById("section-intro");  
+
+  let sticky = navbar.offsetTop;
+  console.log(sectionIntro.offsetTop);
+
+  function myFunction() {
+    if (window.pageYOffset >= sectionIntro.offsetTop) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+})();
