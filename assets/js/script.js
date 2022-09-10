1d5635b201a545ad29839d178b64ab4ebd29c6db
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-10 14:57:41 +0300

    add a second navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 (function () {
-  // window.onscroll = function () {
-  //   manipulateStickyNav();
-  // };
+  window.onscroll = function () {
+    manipulateStickyNav();
+  };
 
   let navbar = document.getElementById("nav");
-  let sectionIntro = document.getElementById("section-intro"); 
-  let introtext = document.getElementById("intro-text"); 
- let introtextPos = introtext.getBoundingClientRect().top;
-  let sticky = navbar.offsetTop;
+  let navbar2 = document.getElementById("nav2");
+  let introtext = document.getElementById("intro-text");
+  let introtextPos = introtext.getBoundingClientRect().bottom;
 
-  let condition = introtextPos + window.scrollY;
-  
-  function manipulateStickyNav() {    
-    if (condition >= 500) {
-      navbar.classList.add("sticky");
-    }else {
-      navbar.classList.remove("sticky");
+  function manipulateStickyNav() {
+    console.log(introtextPos + scrollY);
+    let condition = introtextPos + scrollY;
+    if(condition <= 589){
+      navbar.classList.add('sticky')
+      navbar2.classList.add('hidden');
     }
-    console.log(sticky);
-    console.log(introtextPos + window.scrollY);
+    else{
+      navbar.classList.remove('sticky');
+      navbar2.classList.remove('hidden');
+    }
+   
   }
 })();
