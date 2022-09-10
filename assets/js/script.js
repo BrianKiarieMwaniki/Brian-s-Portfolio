ac28b3510f5f570273f3a286f8f883a795d72e12
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-09-10 15:21:47 +0300

    improve nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,15 @@
 
   let navbar = document.getElementById("nav");
   let navbar2 = document.getElementById("nav2");
-  let introtext = document.getElementById("intro-text");
-  let introtextPos = introtext.getBoundingClientRect().bottom;
 
   function manipulateStickyNav() {
-    console.log(introtextPos + scrollY);
     let condition = introtextPos + scrollY;
-    if(condition <= 589){
-      navbar.classList.add('sticky')
-      navbar2.classList.add('hidden');
+    if (condition <= 589) {
+      navbar.classList.add("sticky");
+      navbar2.classList.add("hidden");
+    } else {
+      navbar.classList.remove("sticky");
+      navbar2.classList.remove("hidden");
     }
-    else{
-      navbar.classList.remove('sticky');
-      navbar2.classList.remove('hidden');
-    }
-   
   }
 })();
